f0efc00986025aa7b63e597089dd70b83b00e623
iamjason <jasongarrett@Jasons-MacBook-Pro.local>
2015-02-05 20:19:02 -0500

    added Xcode Project

---
+++ MavenKit/Fonts.swift
@@ -0,0 +1,54 @@
+//
+//  Fonts.swift
+//  Maven
+//
+//  Created by Jason Garrett on 9/29/14.
+//  Copyright (c) 2014 Maven Clinic. All rights reserved.
+//
+
+import UIKit
+
+
+
+//class Fonts {
+  
+public extension UIFont {
+
+  class func MVN_RalewayRegular(size:CGFloat) -> UIFont {
+    return UIFont(name: "Raleway-Regular", size: size )!
+  }
+  
+  class func MVN_RalewayMedium(size:CGFloat) -> UIFont {
+    return UIFont(name: "Raleway-Medium", size: size )!
+  }
+  
+  class func MVN_RalewaySemiBold(size:CGFloat) -> UIFont {
+    return UIFont(name: "Raleway-SemiBold", size: size )!
+  }
+  
+  class func MVN_RalewayBold(size:CGFloat) -> UIFont {
+    return UIFont(name: "Raleway-Bold", size: size )!
+  }
+  
+  class func MVN_RalewayLight(size:CGFloat) -> UIFont {
+    return UIFont(name: "Raleway-Light", size: size)!
+  }
+  
+  class func MVN_RalewayExtraLight(size:CGFloat) -> UIFont {
+    return UIFont(name: "Raleway-ExtraLight", size: size)!
+  }
+  
+  class func MVN_ProximaNova(size:CGFloat) -> UIFont {
+    return UIFont(name: "ProximaNova-Regular", size: size)!
+  }
+  
+  class func MVN_ProximaNovaSemibold(size:CGFloat) -> UIFont {
+    return UIFont(name: "ProximaNova-Semibold", size: size)!
+  }
+  
+  
+  class func MVN_ProximaNovaLight(size:CGFloat) -> UIFont {
+    return UIFont(name: "ProximaNova-Light", size: size)!
+  }
+  
+}
---
+++ MavenKit/MavenKit.h
@@ -0,0 +1,19 @@
+//
+//  MavenKit.h
+//  MavenKit
+//
+//  Created by Jason Garrett on 1/8/15.
+//  Copyright (c) 2015 Maven Clinic Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MavenKit.
+FOUNDATION_EXPORT double MavenKitVersionNumber;
+
+//! Project version string for MavenKit.
+FOUNDATION_EXPORT const unsigned char MavenKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MavenKit/PublicHeader.h>
+
+
